f0641b738d3866c7469ce0406fa4dc2b205f0c73
Adrian Osak <osak.adrian.k@gmail.com>
2022-07-11 12:16:37 +0200

    toggleHideDone interaction added

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,13 +1,16 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button className="buttons__hidden">
+                <button
+                    onClick={toggleHideDone}
+                    className="buttons__hidden">
                     {hideDone ? "Show" : "Hide"} done
                 </button>
-                <button className="buttons__hidden"
+                <button
+                    className="buttons__hidden"
                     disabled={tasks.every(({ done }) => done)}
                 >
                     Mark all as done
