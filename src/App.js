66d8da1a0bb0de6f487ea3626049105b12650f77
Adrian Osak <osak.adrian.k@gmail.com>
2022-07-11 12:36:56 +0200

    removeTask interaction added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Header from "./Header";
 import Container from "./Container";
 import { useState } from "react";
 
-const tasks = [
-  { id: 1, content: "przejść na Reacta", done: false },
-  { id: 2, content: "odwieźć Paulę do pracy", done: true },
-];
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "przejść na Reacta", done: false },
+    { id: 2, content: "odwieźć Paulę do pracy", done: true },
+  ]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
   return (
@@ -31,6 +35,7 @@ function App() {
           <Tasks
             tasks={tasks}
             hideDone={hideDone}
+            removeTask={removeTask}
           />}
         extraHeaderContent={
           <Buttons
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,21 +1,27 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDone}) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
     <ul className="tasks">
         {tasks.map(task => (
-            <li 
-            key={task.id}
-            className={`tasks__item${task.done && hideDone ? " task__item--hidden" : ""}`}
+            <li
+                key={task.id}
+                className={`tasks__item${task.done && hideDone ? " task__item--hidden" : ""}`}
             >
-            <button className="tasks__button tasks__button--toggleDone">
-            <i className="fa-solid fa-check">
-            </i>
-            </button>
-            <span className={`task__content${task.done ? " task__content--done" : ""}`}>{task.content}</span>
-            <button className="tasks__button tasks__button--delete">
-            <i className="fa-regular fa-trash-can">
-            </i>
-            </button>
+                <button
+                    className="tasks__button tasks__button--toggleDone">
+                    <i className="fa-solid fa-check">
+                    </i>
+                </button>
+                <span
+                    className={`task__content${task.done ? " task__content--done" : ""}`}>{task.content}
+                </span>
+                <button
+                    className="tasks__button tasks__button--delete"
+                    onClick={() => removeTask(task.id)}
+                >
+                    <i className="fa-regular fa-trash-can">
+                    </i>
+                </button>
             </li>
         ))}
     </ul>
